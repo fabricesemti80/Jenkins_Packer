1d650e84870a2993878caddd99c5a671faf0e22a
Fabrice Semti <emilfabrice@gmail.com>
2021-02-18 12:55:59 +0000

    deploy test #12 new ISO and MAK test #4

--- sources/localScripts/update_pkey_2019.ps1
+++ sources/localScripts/update_pkey_2019.ps1
@@ -1,12 +1,13 @@
 $edition = (Get-WmiObject -Class Win32_OperatingSystem).Caption
 
 if ($edition -like '*2019*') {
-    $cName = Get-ComputerName
+    $cName = hostname
     Write-Output 'Applying workaround for 2019 ---> replace KMS key to MAK key'
     # export pre-aut info
     cscript C:\Windows\System32\slmgr.vbs /dli > $('c:\' + $cName + '_pre_activation.txt')
     # remove KMS
     slmgr /upk
+    Start-Sleep -Seconds 30
     # add MAK
     slmgr /ipk 7FDQQ-NJWP6-YFXJ8-HDC9V-MBKRD
     $cName = Get-ComputerName
