7fc4eb87f819dfbaaaac12d5a5368213339e2ac5
Fabrice Semti <emilfabrice@gmail.com>
2021-02-21 09:20:57 +0000

    paralell test #5

--- PACKER-Builder.ps1
+++ PACKER-Builder.ps1
@@ -5,34 +5,19 @@ param(
     [Parameter(Mandatory)]
     $vCenterPwd,
     [Parameter(Mandatory)]
-    # $localUser,
-    # [Parameter(Mandatory)]
     $localPwd,
     [Parameter(Mandatory = $false)][switch] $deploy # if this switch is not enabled, the process will only validate
 )
-
-# If (Get-Module -Name 'Start-parallel') {
-# Get-Module 'start-parallel' -ListAvailable | Import-Module -Verbose -Force
-# }
-# else {
-#     Install-Module 'Start-parallel' -Force
-# }
-
 $timeStamp = Get-Date -Format yyyy-MM-dd
 $note = "This template was created on $timeStamp using Packer!"
-
-
-
+### * ---> PARALELL BUILD --->
 foreach ($cluster in $clusters) {
-
     $inerClusters = $null
     $inerNote = $null
     $inerLocalPwd = $null
     $inervCenterPwd = $null
     $inerDeploy = $null
-
     $builds | ForEach-Object -Parallel {
-
         $inerCluster = $using:cluster
         $inerNote = $using:note
         $inerLocalPwd = $using:localPwd
@@ -40,38 +25,29 @@ foreach ($cluster in $clusters) {
         $inerDeploy = $using:Deploy
         function New-PACKERTemplate {
             param (
-        
                 [Parameter(Mandatory )]
                 [string]
                 $cluster,
-
                 [Parameter(Mandatory )]
                 [string]
                 $note,  
-                
                 [Parameter(Mandatory )]
                 [string]
                 $localPwd, 
-
                 [Parameter(Mandatory )]
                 [string]
                 $vCenterPwd, 
-
                 [Parameter(Mandatory )]
                 [string]
                 $buildName,
-        
                 [Parameter(Mandatory = $false)][switch] $deploy # if this switch is not enabled, the process will only validate        
             )
             Set-Location $buildname 
             $vmName = $($cluster + '_' + $buildName )
-
             $loc = (Get-Location)        
             Write-Output "Entered into folder $loc"
-    
             $date = Get-Date -Format yyyyMMdd
             $buildJSON = '.\' + $date + '_' + $cluster + '_build.json'
-    
             # # Base Image
             if ($deploy.ispresent) {
                 # build the template
@@ -79,8 +55,7 @@ foreach ($cluster in $clusters) {
                 Write-Output "Deploying build: $buildName"
                 Write-Output "Deployment cluster: $cluster"
                 Write-Output "Template file: $buildJSON"
-
-                Start-Process -FilePath 'packer.exe' -ArgumentList "build -timestamp-ui -force -var `"vm-note=$($note)`" -var `"vm_name=$($vmName)`" -var `"vsphere-user=$($vCenterAdmin)`" -var `"vsphere-password=$($vCenterPwd)`" -var `"winadmin-password=$($localPwd)`" $buildJSON" -Wait -NoNewWindow
+                Start-Process -FilePath 'packer.exe' -ArgumentList "build -timestamp-ui -force -var `"vm-note=$($note)`" -var `"vm_name=$($vmName)`" -var `"vsphere-user=$($vCenterAdmin)`" -var `"vsphere-password=$($vCenterPwd)`" -var `"winadmin-password=$($localPwd)`" $buildJSON" -Wait -NoNewWindow -PassThru
             }
             else {
                 # validate the template
@@ -88,44 +63,30 @@ foreach ($cluster in $clusters) {
                 Write-Output "Validating build: $buildName"
                 Write-Output "Deployment cluster: $cluster"
                 Write-Output "Template file: $buildJSON"
-
-                Start-Process -FilePath 'packer.exe' -ArgumentList "validate -var `"vm-note=$($note)`" -var `"vm_name=$($vmName)`" -var `"vsphere-user=$($vCenterAdmin)`" -var `"vsphere-password=$($vCenterPwd)`" -var `"winadmin-password=$($localPwd)`" $buildJSON" -Wait -NoNewWindow
+                Start-Process -FilePath 'packer.exe' -ArgumentList "validate -var `"vm-note=$($note)`" -var `"vm_name=$($vmName)`" -var `"vsphere-user=$($vCenterAdmin)`" -var `"vsphere-password=$($vCenterPwd)`" -var `"winadmin-password=$($localPwd)`" $buildJSON" -Wait -NoNewWindow -PassThru
             }
             # exited folder
             Set-Location ..
             $loc = Get-Location
             Write-Output "Entered into folder $loc"    
-    
         }
-
         Write-Output "PROCESSING --> $($_)"
-
         if ($inerDeploy.IsPresent) {
             New-PACKERTemplate -cluster $inerCluster -note $inerNote -localpwd $inerLocalPwd -vcenterpwd $inervCenterPwd -buildName $($_) -deploy
         }
         else {
             New-PACKERTemplate -cluster $inerCluster -note $inerNote -localpwd $inerLocalPwd -vcenterpwd $inervCenterPwd -buildName $($_)
         }
-        
-
-        
     }
-
- 
 }
-
-
+### * ---> SEQUENTIAL BUILD --->
 # foreach ($cluster in $clusters) {
 #     foreach ($buildName in $builds) {
-    
-
 #         Set-Location $buildname 
 #         $loc = Get-Location
 #         Write-Output "Entered into folder $loc"
-    
 #         $date = Get-Date -Format yyyyMMdd
 #         $buildJSON = '.\' + $date + '_' + $cluster + '_build.json'
-
 #         # switch ($buildName) {
 #         #     '2019_core' {
 #         #         if ($deploy.ispresent) {
@@ -134,8 +95,6 @@ foreach ($cluster in $clusters) {
 #         #         else {
 #         #             Write-Output 'We are validating  template -> 2019 core server'  
 #         #         }
-        
-    
 #         #     } 
 #         #     '2019_gui' {
 #         #         if ($deploy.ispresent) {
@@ -152,7 +111,6 @@ foreach ($cluster in $clusters) {
 #         #         Write-Output 'We are building  templates -> 2019 desktop server'
 #         #     }
 #         # }
-    
 #         # # Base Image
 #         if ($deploy.ispresent) {
 #             # build the template
@@ -160,7 +118,6 @@ foreach ($cluster in $clusters) {
 #             Write-Output "Deploying build: $buildName"
 #             Write-Output "Deployment cluster: $cluster"
 #             Write-Output "Template file: $buildJSON"
-
 #             Start-Process -FilePath 'packer.exe' -ArgumentList "build -timestamp-ui -force -var `"vm-note=$($note)`" -var `"vm_name=$($buildName)`" -var `"vsphere-user=$($vCenterAdmin)`" -var `"vsphere-password=$($vCenterPwd)`" -var `"winadmin-password=$($localPwd)`" $buildJSON" -Wait -NoNewWindow
 #         }
 #         else {
@@ -169,7 +126,6 @@ foreach ($cluster in $clusters) {
 #             Write-Output "Validating build: $buildName"
 #             Write-Output "Deployment cluster: $cluster"
 #             Write-Output "Template file: $buildJSON"
-
 #             Start-Process -FilePath 'packer.exe' -ArgumentList "validate -var `"vm-note=$($note)`" -var `"vm_name=$($buildName)`" -var `"vsphere-user=$($vCenterAdmin)`" -var `"vsphere-password=$($vCenterPwd)`" -var `"winadmin-password=$($localPwd)`" $buildJSON" -Wait -NoNewWindow
 #         }
 #         # exited folder
@@ -177,9 +133,4 @@ foreach ($cluster in $clusters) {
 #         $loc = Get-Location
 #         Write-Output "Entered into folder $loc"
 #     }
-
- 
-# }
-
-
-
+#}
