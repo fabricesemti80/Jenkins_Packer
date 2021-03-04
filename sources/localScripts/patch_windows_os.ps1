b93accc5d6ddb8cea4ba9b23e5351ce859c25b02
Fabrice Semti <emilfabrice@gmail.com>
2021-03-04 10:38:45 +0000

    changed to Windows Update provisioner - https://tinyurl.com/ymemjm6k

--- sources/localScripts/patch_windows_os.ps1
+++
@@ -1,6 +0,0 @@
-$sName = hostname
-
-Write-Output "$sname - STARTING WINDOWS PATCHES"
-Get-WUInstall -WindowsUpdate -AcceptAll -IgnoreReboot -Download -Install -Confirm:$false -Verbose # * get updates from our WSUS
-Get-WUInstall -MicrosoftUpdate -AcceptAll -Download -Install -IgnoreReboot -Confirm:$false -Verbose # * get updates from Microsoft 
-Write-Output "$sname - COMPLETED WINDOWS PATCHES"
