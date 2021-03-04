215fa696f73a9a5f3b94b1e6b862ce41bae99919
Fabrice Semti <emilfabrice@gmail.com>
2021-03-04 09:37:38 +0000

    added windows patch script #2

--- sources/localScripts/patch_windows_os.ps1
+++ sources/localScripts/patch_windows_os.ps1
@@ -1,6 +1,6 @@
 $sName = hostname
 
 Write-Output "$sname - STARTING WINDOWS PATCHES"
-Get-WUInstall -WindowsUpdate -AcceptAll -IgnoreReboot -Download -Install -Confirm:$false -ForceDonwload -ForceInstall -Verbose # * get updates from our WSUS
-Get-WUInstall -MicrosoftUpdate -AcceptAll -Download -Install -IgnoreReboot -Confirm:$false -ForceDonwload -ForceInstall -Verbose # * get updates from Microsoft 
+Get-WUInstall -WindowsUpdate -AcceptAll -IgnoreReboot -Download -Install -Confirm:$false -Verbose # * get updates from our WSUS
+Get-WUInstall -MicrosoftUpdate -AcceptAll -Download -Install -IgnoreReboot -Confirm:$false -Verbose # * get updates from Microsoft 
 Write-Output "$sname - COMPLETED WINDOWS PATCHES"
